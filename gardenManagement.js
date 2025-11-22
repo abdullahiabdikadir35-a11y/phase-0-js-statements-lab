6a6fd7e0405d1078c9f67acac6c52c4f32944932
abdullahiabdikadir35-a11y <abdullahiabdikadir35@gmail.com>
2025-11-22 15:43:54 +0300

    completed gardenManagment lab

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,19 @@
-const temperature = 90;
-const timeOfDay = "morning";
+let temperature = 85;
+
+if (temperature > 80) {
+  console.log("Watering on");
+}
+
+let timeOfDay = "morning";
+
+if (timeOfDay === "morning") {
+  console.log("Lights off");
+}
+
 let soilMoisture = 30;
+
+while (soilMoisture < 40) {
+  soilMoisture++;
+}
+
+console.log(soilMoisture);
